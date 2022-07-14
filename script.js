ba69bd411de40ef1470727818d76ba16a49b19e9
Simon Reinhardt <saadabou.study@gmail.com>
2022-07-14 15:12:38 +0100

    working on the drop down accessibility menu

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ let processScroll = () => {
 
 document.addEventListener('scroll', processScroll);
 
+
+// accessibility
+
+function accessibility() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.header__access-button')) {
+        var dropdowns = document.getElementsByClassName("header__access-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
