44a026a334c9c84096fd0a2619abc94799d50383
Simon Reinhardt <saadabou.study@gmail.com>
2022-07-25 12:56:46 +0100

    fixing issues

--- script.js
+++ script.js
@@ -14,20 +14,23 @@ document.addEventListener('scroll', processScroll);
 // accessibility
 
 function accessibility() {
-    document.getElementById("dropdown").classList.toggle("show");
+    var dropdown = document.getElementById("dropdown");
+    dropdown.classList.toggle("show");
 }
 
+
+
+
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.header__access-button')) {
-        
-        var dropdowns = document.getElementsByClassName("header__access-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
+// window.onclick = function (event) {
+//     if (!event.target.matches('.header__access-button')) {
+//         var dropdowns = document.getElementsByClassName("header__access-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
