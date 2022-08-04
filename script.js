91bf5dfe8021839440f2ce5095b39f5e8d162731
Simon Reinhardt <saadabou.study@gmail.com>
2022-08-04 12:13:03 +0100

    hide and reveal access bar

--- script.js
+++ script.js
@@ -10,5 +10,28 @@ let processScroll = () => {
 
 document.addEventListener('scroll', processScroll);
 
+let accessBar = document.getElementById("accessBar");
+let accessBtn = document.getElementById("accessBtn");
+let closeBtn = document.getElementById("closeBtn");
 
+accessBtn.addEventListener("click", function(){
+    accessBar.classList.add("reveal");
+    accessBar.classList.remove("hideBar");
 
+    setTimeout(() => {
+        accessBar.style.display = "flex";
+        accessBtn.style.display = "none";
+        closeBtn.style.display = "block";
+    }, 300);
+})
+
+closeBtn.addEventListener("click", function(){
+    accessBar.classList.add("hideBar");
+    accessBar.classList.remove("reveal");
+
+    setTimeout(() => {
+        accessBar.style.display = "none";
+        accessBtn.style.display = "block";
+        closeBtn.style.display = "none";
+    }, 200);
+})
